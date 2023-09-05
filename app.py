cdc8d8b5cfb4869f54e4f3d275cf345eb1cd7bf7
JJulme <jung_min822@nate.com>
2023-09-05 14:20:10 +0900

    코드를 해석해서 가져다 주는도록 수정

--- app.py
+++ app.py
@@ -4,22 +4,29 @@ from flask_cors import CORS
 import csv_test as df
 
 app = Flask(__name__)
+# 한글 깨짐 현상 해결
+app.config["JSON_AS_ASCII"]=False
 # CORS 설정을 해줘야 클라이언트에서 접속 가능, 하지만 보안 취약
 cors = CORS(app, resources={r"*":{"origins":"*"}})
 
 # csv 데이터 가져오기
 themepark_recent = df.themepark_recent()
 themepark_basic = df.themepark_basic()
+themepark_place = ["에버랜드", "롯데월드", "서울랜드"]
+themepark_kategorie = ["운영시간", "공연일정", "편의시설", "장애인 편의시설", "가족 편의시설", "요금우대", "음식점", "대중교통 이용방법"]
 
 # 기본 로컬 설정
 @app.route("/")
 def hello():
     return "Hello, my Flask!"
 
-@app.route("/themepark/<date>/<place>/<kategorie>")
-def get_themepark(date, place, kategorie):
-    result = df.get_info(themepark_recent, themepark_basic, place, kategorie, int(date))
+# 크롤링 데이터와 기본 정보 조회해서 결과 보여줌
+@app.route("/themepark/<date>/<code>")
+def get_themepark(date, code):
+    check_list = df.get_code(themepark_place, themepark_kategorie, code)
+    result = df.get_data(themepark_recent, themepark_basic, int(date), check_list)
     return result
+    
 
 # python app.py 서버 열기
 if __name__ == "__main__":
--- csv_test.py
+++ csv_test.py
@@ -21,3 +21,41 @@ def get_info(recent, basic, place:str, kategorie:str, date=2023):
         # Series 형식을 String 형식으로 변환
         result = ", ".join(data.values)
         return result
+    
+def get_code(place:list, kategorie:list, code:str):
+    code_list = code.split("_")
+    place_code = list(code_list[0])
+    kategorie_code = list(code_list[1])
+    check_place = []
+    check_kategorie=[]
+    for i in range(len(place_code)):
+        if place_code[i]=="0":
+            pass
+        else:
+            check_place.append(place[i])
+    for i in range(len(kategorie_code)):
+        if kategorie_code[i]=="0":
+            pass
+        else:
+            check_kategorie.append(kategorie[i])
+
+    result = [check_place, check_kategorie]
+    return result
+
+def get_data(recent, basic, day, check_list):
+    check_place = check_list[0]
+    check_kategorie = check_list[1]
+    list_large = []
+    for kategorie in check_kategorie:
+        list_small = []
+        for place in check_place:
+            try:
+                data = recent.loc[(recent["날짜"]==day) & (recent["장소"]==place)][kategorie]
+            except:
+                data = basic.loc[(basic["장소"]==place)][kategorie]
+            finally:
+                # Series 형식을 String 형식으로 변환
+                result = ", ".join(data.values)
+                list_small.append(result)
+        list_large.append(list_small)
+    return list_large
